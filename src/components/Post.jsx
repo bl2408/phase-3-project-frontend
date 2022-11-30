f70fdc9470844a268705028d132c27bafded87df
Brian Lambert <brianlambert2408@gmail.com>
2022-11-30 22:21:12 +1100

    Added current user post highlights

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,16 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AppContext } from "./App";
 
 export default function Post({data}){
 
+    const { appState } = useContext(AppContext);
+    const { loggedIn, userData } = appState;
+    const isAuthor = userData?.name?.toLowerCase() === data.author.name.toLowerCase();
+
     return (
         <article style={{margin: "10px 0 20px 0"}}>
-            <header>
+            <header style={isAuthor ? {backgroundColor:"rgba(0,0,0,0.5)"} : {}}>
                 {data.title} <br />
                 {data.created_at} : {data.updated_at}<br />
                 <Link to={`/users/${data.author.name}`}>{data.author.name}</Link>
