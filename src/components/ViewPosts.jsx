1064ecc6cef0c83c45f33eb617bbc1819fc87033
Brian Lambert <brianlambert2408@gmail.com>
2022-12-12 17:11:41 +1100

    Added token to search

--- src/components/ViewPosts.jsx
+++ src/components/ViewPosts.jsx
@@ -6,6 +6,7 @@ import { v4 as uuid } from 'uuid';
 import Post from './Post';
 import { ep } from '../data/endpoints';
 import { useParams } from 'react-router-dom';
+import { useRef } from 'react';
 
 
 
@@ -89,8 +90,62 @@ export default function ViewPosts(){
         }
     };
 
+    const searchTxt = useRef();
+
+    const handleSearch=(e)=>{
+        e.preventDefault();
+
+        const text = searchTxt.current.value;
+
+        const headerObj = {
+            "Content-Type": "application/json",
+        };
+
+        if(loggedIn){
+            headerObj.token = appState.token
+        }
+
+        const requestObj = {
+            method: "GET",
+            headers: headerObj
+        }
+
+        fetch(`${ep.postsAll()}?search=${text}`,requestObj)
+        .then(res=>res.json())
+        .then(data=>{
+
+            if(data.success){
+                setPosts(d=>data.results.map(post=>{
+                    const postObj = {...post};
+                    postObj.readTime = wordCountTime(postObj.body);
+                    postObj.viewMode = "all";
+                    postObj.body = `${postObj.body.substring(0,200)}...`
+                    
+                    return postObj
+                }))
+            }else{
+                setPosts(d=>({message: `No results found: ${text}`}))
+            }
+
+        });
+
+    };
+
     return (
         <section>
+
+            {!!id || !!userId ? null:
+                <form onSubmit={handleSearch}>
+                    <div style={{display: "grid", gridTemplateColumns:"auto max-content max-content"}}>
+                        <input type="text" ref={searchTxt} onChange={e=>searchTxt.current.value = e.target.value}/>
+                        <input type="submit" value="search" />
+                        <input type="reset" value="x" />
+                    </div>
+                </form>
+            }
+
+            
+
             { viewPosts() }
         </section>
     );
